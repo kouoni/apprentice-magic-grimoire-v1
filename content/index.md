a3f413b6878584379bedb6d90d2faf11203b2bfc
need-fish <kiet.swe@gmail.com>
2023-12-31 23:43:14 -0700

    Quartz sync: Dec 31, 2023, 11:43 PM

--- content/index.md
+++ content/index.md
@@ -3,11 +3,11 @@ title: witch's introduction notes
 ---
 Welcome!
 
-If you are reading this, then you must have known me in some capacity.
+If you are reading this, then you must have met me in some way.
 
 I wrote this as a way to document what I am going to learn in 2024, as one with zero hands-on creative skills, and the internet as my only teacher.
 
-It is themed as a magic book, because I read this manga called Witch Hat Atelier some time ago and was very inspired. Plus, "magic" sounds much more creative than simply "music" or "art". I view them as magical practice. And instead of "amateur", "apprentice witch" sounds way more fun.
+It is themed as a magic book, because I read this manga called [Witch Hat Atelier](https://mangadex.org/title/67e7453b-9ee5-4ae5-9316-215b03e4a71d/witch-hat-atelier?tab=chapters) some time ago and was very inspired. Plus, "magic" sounds much more creative than simply "music" or "art". I view them as magical practice. And instead of "amateur", "apprentice witch" sounds way more fun.
 
 The notes I post here will be all my thoughts regarding the learning of new artistic and creative skills, be it preparation, pathways, assistance, resources I know of, motivation I gathered, troubles I can imagine. I hope that you will find my notes useful in helping you on your own path.
 
--- quartz.config.ts
+++ quartz.config.ts
@@ -3,39 +3,37 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
-    enableSPA: true,
-    enablePopovers: true,
-    analytics: {
-      provider: "plausible",
-    },
-    baseUrl: "quartz.jzhao.xyz",
+    pageTitle: "kitan's Apprentice Magic Grimoire",
+    enableSPA: false,
+    enablePopovers: false,
+    analytics: null,
+    baseUrl: "kitans-grimoire.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
-        code: "IBM Plex Mono",
+        header: "DM Serif Text",
+        body: "Fira Code",
+        code: "Fira Code",
       },
       colors: {
         lightMode: {
-          light: "#faf8f8",
-          lightgray: "#e5e5e5",
+          light: "#e7e6df",
+          lightgray: "#e0dcd3",
           gray: "#b8b8b8",
           darkgray: "#4e4e4e",
           dark: "#2b2b2b",
-          secondary: "#284b63",
+          secondary: "#7d9726",
           tertiary: "#84a59d",
           highlight: "rgba(143, 159, 169, 0.15)",
         },
         darkMode: {
-          light: "#161618",
+          light: "#302f27",
           lightgray: "#393639",
           gray: "#646464",
           darkgray: "#d4d4d4",
-          dark: "#ebebec",
-          secondary: "#7b97aa",
+          dark: "#e7e6df",
+          secondary: "#ba6236",
           tertiary: "#84a59d",
           highlight: "rgba(143, 159, 169, 0.15)",
         },
